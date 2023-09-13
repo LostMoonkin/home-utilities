0b32432f32f2b0d51a700a909eb4467b57525e71
moonkin@pc <ymbn01@hotmail.com>
2023-09-13 09:46:26 +0800

    feat: finish modify wifi channel scripts

--- scripts/modify-wifi-channel/config.py
+++ scripts/modify-wifi-channel/config.py
@@ -1,4 +1,5 @@
 Config = {
-    "host": "192.168.1.90",
-    "auth_password": "4U8L0JM02wefbwK"
+    "router_host": "192.168.1.90",
+    "auth_password": "4U8L0JM02wefbwK",
+    "ipc_host": "192.168.1.80"
 }
