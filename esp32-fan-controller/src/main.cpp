9be55ac8775d2d81cf419b32a71cb05a0d1e3d79
moonkin@pc <ymbn01@hotmail.com>
2023-10-30 13:04:21 +0800

    feat: add esp32 hep output

--- esp32-fan-controller/src/main.cpp
+++ esp32-fan-controller/src/main.cpp
@@ -2,6 +2,10 @@
 #include "setup_wifi.h"
 
 void setup() {
+    Serial.begin(115200);
+    Serial.printf("Deafult free size: %d\n", heap_caps_get_free_size(MALLOC_CAP_DEFAULT));
+    Serial.printf("PSRAM free size: %d\n", heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
+    Serial.printf("Flash size: %d\n", ESP.getFlashChipSize());
 // write your initialization code here
 }
 
